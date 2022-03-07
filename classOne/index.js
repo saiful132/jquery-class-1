e3bc670e29c17ff67f98df95718e806ce9ab457b
Md-Saifullah <rssaiful67@gmail.com>
2022-03-07 18:32:01 +0600

    added some jquery mathod

--- classOne/index.js
+++ classOne/index.js
@@ -1,2 +1,11 @@
 // document.querySelector(".demo").innerHTML = "The text changed";
 $(".demo").text("Hello I am changed using jquery");
+$(".div1 h1, #demo2").text("I am changed from jquery");
+$("#demo3").html("<u> Rony Begum </u>");
+$(".demo4").append(" Rony");
+$(".demo5").prepend("Rony and ");
+
+var para0 = $("<p></p>").text("This is paragraph 0");
+$(".para1").before(para0);
+var para2 = $("<p></p>").text("This is paragraph 2");
+$(".para1").after(para2);
