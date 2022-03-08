0e0d720a79c5423966d0a298ad868bccd16e20d9
Md-Saifullah <rssaiful67@gmail.com>
2022-03-09 00:59:38 +0600

    final and last changed

--- classOne/index.js
+++ classOne/index.js
@@ -3,5 +3,24 @@ function example1() {
 }
 
 function example2() {
-	$(".demo2").html("<b> I am changed from jQuery code </b>");
+	$(".demo2").html("<b> I am changed from jQuery code and I am bold </b>");
+}
+
+function example3() {
+	$(".demo3").append(" (I am added in the end position)");
+}
+
+function example4() {
+	$(".demo4").prepend(" (I am added in the start position)");
+}
+
+var beforeP = $("<p></p>").text("I am an element added before the text");
+
+function example5() {
+	$(".demo5").before(beforeP);
+}
+
+var afterP = $("<p></p>").text("I am an element added after the text");
+function example6() {
+	$(".demo6").after(afterP);
 }
