78c17209e8d21785d9f415116bc788ffb3de69a4
Md-Saifullah <rssaiful67@gmail.com>
2022-03-08 12:07:00 +0600

    changed hole code

--- classOne/index.js
+++ classOne/index.js
@@ -1,11 +1,7 @@
-// document.querySelector(".demo").innerHTML = "The text changed";
-$(".demo").text("Hello I am changed using jquery");
-$(".div1 h1, #demo2").text("I am changed from jquery");
-$("#demo3").html("<u> Rony Begum </u>");
-$(".demo4").append(" Rony");
-$(".demo5").prepend("Rony and ");
+function example1() {
+	$(".demo1").text("I am changed from jQuery code");
+}
 
-var para0 = $("<p></p>").text("This is paragraph 0");
-$(".para1").before(para0);
-var para2 = $("<p></p>").text("This is paragraph 2");
-$(".para1").after(para2);
+function example2() {
+	$(".demo2").html("<b> I am changed from jQuery code </b>");
+}
